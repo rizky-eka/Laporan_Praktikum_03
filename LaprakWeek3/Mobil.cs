940a8b5ccec527ab4669d59a683ac97c11ef2bf3
rizky eka haryadi <rizky28eka@MacBook-Pro-rizky.local>
2023-03-22 15:22:29 +0700

    first commit

---
+++ LaprakWeek3/Mobil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace LaporanPraktikum_3
+{
+    public class Mobil
+    {
+        public string warna;
+        public int JumlahPintu;
+        public string merek;
+        public string model;
+        public int TahunKeluar;
+
+
+        public void Gas(int kecepatan)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", model, kecepatan);
+        }
+
+        public void Klakson(string suara)
+        {
+            Console.WriteLine("{0}", suara);
+        }
+
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Mobil saya berwana {0}, merek {1} model {2} keluaran tahun {3} dengan jumlah pintu sebanyak {4}", warna, merek, model, TahunKeluar, JumlahPintu);
+        }
+
+    }
+}
---
+++ LaprakWeek3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace LaporanPraktikum_3
+
+{
+    public class MobilApp
+    {
+        static public void Main(String[] args)
+        {
+            Mobil mobil1 = new Mobil();
+            mobil1.warna = "Purple";
+            mobil1.JumlahPintu = 2;
+            mobil1.merek = "Angkot";
+            mobil1.model = "Angkutan Umum";
+            mobil1.TahunKeluar = 1945;
+
+            mobil1.Gas(300);
+            mobil1.Klakson("DJ Angkot");
+            mobil1.TampilkanInfo();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("===================");
+
+            Mobil mobil2 = new Mobil();
+            mobil2.warna = "Pink";
+            mobil2.JumlahPintu = 4;
+            mobil2.merek = "Taxi";
+            mobil2.model = "Angkutan Umum";
+            mobil2.TahunKeluar = 2030;
+
+            mobil2.Gas(200);
+            mobil2.Klakson("Tetttt tettttt");
+            mobil2.TampilkanInfo();
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
